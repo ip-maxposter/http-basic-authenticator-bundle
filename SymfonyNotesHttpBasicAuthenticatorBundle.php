ebfcb0c56f36ea0d889add595957d688d8492633
Roman Zhuravel <roman.zhooravell@tonicforhealth.com>
2017-03-14 18:17:11 +0200

    add bundle extension

--- SymfonyNotesHttpBasicAuthenticatorBundle.php
+++ SymfonyNotesHttpBasicAuthenticatorBundle.php
@@ -3,10 +3,18 @@
 namespace SymfonyNotes\HttpBasicAuthenticatorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use SymfonyNotes\HttpBasicAuthenticatorBundle\DependencyInjection\HttpBasicAuthenticatorExtension;
 
 /**
  * Class SymfonyNotesHttpBasicAuthenticatorBundle
  */
 class SymfonyNotesHttpBasicAuthenticatorBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        return new HttpBasicAuthenticatorExtension();
+    }
 }
